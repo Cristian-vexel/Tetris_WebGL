f480cecd5b47ef695b8acc71f15c6eb6c48c8e46
Cristian-vexel <80277457+Cristian-vexel@users.noreply.github.com>
2022-05-28 15:11:43 +0300

    Update new site

--- src/viewWebGL.js
+++ src/viewWebGL.js
@@ -99,7 +99,7 @@ export default class View {
         let info2 = document.querySelector('#info2');
         info2.innerHTML = '-----------------------------';
         let info3 = document.querySelector('#info3');
-        info3.innerHTML = 'https://github.com/Konstantin84UKR/Tetris_WebGL';
+        info3.innerHTML = 'https://github.com/Cristian-vexel/Tetris_WebGL';
     }
 
     renderPauseScreen() {
@@ -118,7 +118,7 @@ export default class View {
         let info2 = document.querySelector('#info2');
         info2.innerHTML = '-----------------------------';
         let info3 = document.querySelector('#info3');
-        info3.innerHTML = 'https://github.com/Konstantin84UKR/Tetris_WebGL';
+        info3.innerHTML = 'https://github.com/Cristian-vexel/Tetris_WebGL';
     }
 
 
@@ -140,7 +140,7 @@ export default class View {
         let info2 = document.querySelector('#info2');
         info2.innerHTML = 'Press ENTER to Resume';
         let info3 = document.querySelector('#info3');
-        info3.innerHTML = 'https://github.com/Konstantin84UKR/Tetris_WebGL';
+        info3.innerHTML = 'https://github.com/Cristian-vexel/Tetris_WebGL';
     }
 
 
